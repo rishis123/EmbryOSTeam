6242fc892e63baf5b2c0ede6ffc0fd76b2f61881
Evan C <ec875@cornell.edu>
2026-02-20 19:52:46 -0500

    Add explanation.md

--- chapter12/sched.c
+++ chapter12/sched.c
@@ -2,17 +2,17 @@
 
 #define N_PRIORITIES 3
 
-// This is an array of cricular linked lists of runnable processes. 
-// Each index is a different priority (0 high to 2 low). 
+// This is an array of cricular linked lists of runnable processes.
+// Each index is a different priority (0 high to 2 low).
 // Runs next high priority available process in run_queue. If there is higher priority available, always runs that first.
 struct pcb *run_queue[N_PRIORITIES];
-//Single circular linked list of which processes are sleeping.
+// Single circular linked list of which processes are sleeping.
 struct pcb *sleep_queue = NULL;
 
 // adds this process (encoded in pcb) to runnable queue at high priority. sched_block internally always chooses the highest priority process available to resume.
 void sched_resume(struct pcb *pcb) { proc_enqueue(&run_queue[0], pcb); }
 
-//adds this process to sleep queue.
+// adds this process to sleep queue.
 void sched_sleep(struct pcb *pcb)
 {
     proc_enqueue(&sleep_queue, pcb);
@@ -29,7 +29,7 @@ void sched_block(struct pcb *old)
         struct pcb *curr = sleep_queue->next;
         do
         {
-            //Currently sleeping and deadline is over.
+            // Currently sleeping and deadline is over.
             if (curr->sleeping && curr->sleep_deadline <= curr_time)
             {
                 // need to wake up curr
@@ -37,19 +37,19 @@ void sched_block(struct pcb *old)
 
                 // remove curr from sleep queue
                 prev->next = curr->next;
-                
-                // sleep_queue points to tail of list. 
-                //if removing tail, then result in null queue if only curr, or else prev (element before the old tail)
+
+                // sleep_queue points to tail of list.
+                // if removing tail, then result in null queue if only curr, or else prev (element before the old tail)
                 if (curr == sleep_queue)
                 {
                     sleep_queue = (curr->next == curr) ? NULL : prev;
                 }
-                //we move curr pointer forward, and make another var storing curr called curr_wake, so that we can wake it up.
+                // we move curr pointer forward, and make another var storing curr called curr_wake, so that we can wake it up.
                 struct pcb *curr_wake = curr;
                 curr = curr->next;
 
-                //AI Used to understand where to use sched_resume.
-                // Put curr_wake back on run queue, it is no longer curr because we just moved curr to next
+                // AI Used to understand where to use sched_resume.
+                //  Put curr_wake back on run queue, it is no longer curr because we just moved curr to next
                 sched_resume(curr_wake);
             }
             else
@@ -61,17 +61,18 @@ void sched_block(struct pcb *old)
 
         } while (sleep_queue != NULL && curr != sleep_queue->next);
     }
-    //choose next runnable process
+
+    // choose next runnable process
     int p = 0; // first find highest priority process
     while (p < N_PRIORITIES && run_queue[p] == 0)
         p++;
     struct pcb *new = proc_dequeue(&run_queue[p]);
 
-    //if we aren't changing processes, don't do anything.
+    // if we aren't changing processes, don't do anything.
     if (new != old)
     { // switch needed, basically just let the new process run.
 
-        //hart number since process should run on same as old
+        // hart number since process should run on same as old
         new->hart = old->hart;
         new->hart->needs_tlb_flush = 1;
         sched_set_self(new);
@@ -80,7 +81,8 @@ void sched_block(struct pcb *old)
         reap_zombies();
     }
 }
-//for starting a new user process.
+
+// for starting a new user process.
 void sched_run(int executable, struct rect area, void *args, int size)
 {
     struct pcb *old = sched_self();
@@ -91,10 +93,10 @@ void sched_run(int executable, struct rect area, void *args, int size)
     ctx_start(&old->sp, (char *)new + PAGE_SIZE);
     reap_zombies();
 }
-//running process can invoke to give up CPU and let somebody else run.
+
+// running process can invoke to give up CPU and let somebody else run.
 void sched_yield(void)
 {
-
     struct pcb *current = sched_self();
     if (current->executable > 0)
     { // idle loop doesn't yield at interrupts
